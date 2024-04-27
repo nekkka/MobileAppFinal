e22cf73d5a0212ba6fe2ed97d34d596d1d99668c
Anuar Adilbek <adilbek.anuar@gmail.com>
2024-04-27 19:48:27 +0500

    APIClient

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/finalprojectstocks/model/network/APIClient.kt
+++ app/src/main/java/com/example/finalprojectstocks/model/network/APIClient.kt
@@ -1,11 +1,26 @@
 package com.example.finalprojectstocks.model.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object APIClient {
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request()
+
+            val newRequest = request.newBuilder()
+                .addHeader("X-API-KEY", "7RBTSE/f5E8EBQqb9nDf7A==Vy1UW9ycFb6uXgGv")
+                .build()
+
+            chain.proceed(newRequest)
+        }
+        .build()
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://api.api-ninjas.com/v1/")
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 

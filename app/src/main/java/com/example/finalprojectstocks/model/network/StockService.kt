c6553ce118d14bb781733d45144d6253069990b6
Olzhas <olzhas.taukent@gmail.com>
2024-04-29 17:27:18 +0600

    fixed api

--- app/src/main/java/com/example/finalprojectstocks/model/network/StockService.kt
+++ app/src/main/java/com/example/finalprojectstocks/model/network/StockService.kt
@@ -3,9 +3,10 @@ package com.example.finalprojectstocks.model.network
 import com.example.finalprojectstocks.model.entity.Stocks
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface StockService {
-    @GET("stockprice?x-api-key=IQhgB1xircwnSIg6p2lVKQ==k3DIFrjL15mkXojU")
-    fun fetchKotikList(@Query("name")name: String): Call<List<Stocks>>
+    @GET("stockprice")
+    fun fetchKotikList(@Query("ticker")ticker: String): Call<Stocks>
 }

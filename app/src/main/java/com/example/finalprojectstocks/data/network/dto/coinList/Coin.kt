57054578dd455cae39f670047c4b5b4d56d7e9d5
Anuar Adilbek <adilbek.anuar@gmail.com>
2024-05-16 18:39:44 +0500

    Coin

---
+++ app/src/main/java/com/example/finalprojectstocks/data/network/dto/coinList/Coin.kt
@@ -0,0 +1,26 @@
+package com.example.finalprojectstocks.data.network.dto.coinList
+
+import com.example.finalprojectstocks.domain.models.Coin
+
+
+data class CoinResponse(
+    val item: CoinDTO
+)
+
+data class CoinDTO(
+    val id: String,
+    val coin_id: Int,
+    val name: String,
+    val symbol: String,
+    val thumb: String
+)
+
+
+fun CoinDTO.toCoin(): Coin {
+    return Coin(
+        id = this.id,
+        name = this.name,
+        symbol = this.symbol,
+        thumb = this.thumb
+    )
+}
---
+++ app/src/main/java/com/example/finalprojectstocks/domain/models/Coin.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojectstocks.domain.models
+
+data class Coin(
+    val id: String,
+    val name: String,
+    val symbol: String,
+    val thumb: String
+)
